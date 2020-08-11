892835268d8d24ec99f627962ce0ec43cfcfd360
Odumosu Oluwashina <odumz20@gmail.com>
2020-08-11 02:48:22 +0100

    Initial commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once "class-db.php";
+ 
+define('CLIENT_ID', 'Nv4hdXVHRbeoN5XZTcbw');
+define('CLIENT_SECRET', 'uaxBTRa6ie6VOLEoLDnWgE8bm9REezff');
+define('REDIRECT_URI', '');
